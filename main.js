a7a158434be331d8e9259ceb56468be1efa05faf
Achhu <88780913+Achhu@users.noreply.github.com>
2022-10-13 21:23:10 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+function brother()
+{
+    window.location = "brother.html";
+}
+
+function mother()
+{
+    window.location = "mother.html";
+}
+
+function father()
+{
+window.location = "father.html";
+}
+
+function me()
+{
+    window.location = "ME.html"
+}
